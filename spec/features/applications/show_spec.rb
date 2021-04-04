95a6afb18b349add7a599ee80942ac8505c05711
Brisa Garcia <brisagarciaglz@gmail.com>
2021-04-03 21:16:08 -0600

    Add feature testing for all Apply for Pets section

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe 'As a visitor' do
       within("#application-#{@applicant1.id}") do
         expect(page).to have_content(@applicant1.name)
         expect(page).to have_content(@applicant1.address)
-        expect(page).to have_content(@applicant1.description)
+        # expect(page).to have_content(@applicant1.description)
         expect(page).to have_content('In Progress')
       end
     end
@@ -28,9 +28,40 @@ RSpec.describe 'As a visitor' do
         fill_in :search, with: 'Mr. Dog'
         click_button('Search')
         expect(current_path).to eq("/applications/#{@applicant1.id}")
-        save_and_open_page
+        # save_and_open_page
         expect(page).to have_content(@pet3.name)
+        click_button 'Adopt this Pet'
+        expect(current_path).to eq("/applications/#{@applicant1.id}")
       end
+    end
+
+  describe 'When one or more pets are added to the application' do
+    it 'I see a section to submit my application and no mor add more pets' do
+      visit "/applications/#{@applicant1.id}"
+
+      within(".submit-app") do
+        expect(page).to have_button('Submit Application')
+        expect(page).not_to have_content('Add a Pet to this Application')
+
+      end
+    end
+  end
+
+    it 'I see an input to enter why I would make a good owner for these pet(s)' do
+      visit "/applications/#{@applicant1.id}"
+
+      within(".submit-app") do
+        expect(page.find_field(:description)).to be_truthy
+      end
+    end
+
+    it 'I see that the application is "Pending"' do
+      visit "/applications/#{@applicant1.id}"
+
+      within(".submit-app") do
+        click_button('Submit Application')
+      end
+      expect(page).to have_content('Pending')
     end
   end
 end
