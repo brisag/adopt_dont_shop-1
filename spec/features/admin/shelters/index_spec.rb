3189cbb874af76db661ce0bee79568c27209d2c2
Brisa Garcia <brisagarciaglz@gmail.com>
2021-04-04 11:45:05 -0600

    Test if pending shows up

--- spec/features/admin/shelters/index_spec.rb
+++ spec/features/admin/shelters/index_spec.rb
@@ -6,9 +6,20 @@ RSpec.describe 'As a visitor' do
       @shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
       @shelter_2 = Shelter.create(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5)
       @shelter_3 = Shelter.create(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)
-      @shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
-      @shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
-      @shelter_3.pets.create(name: 'Lucille Bald', breed: 'sphynx', age: 8, adoptable: true)
+      @pet_1 = @shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
+      @pet_2 = @shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
+      @pet_3 = @shelter_3.pets.create(name: 'Lucille Bald', breed: 'sphynx', age: 8, adoptable: true)
+
+      @applicant_1 = Application.create!(name: 'Mike', address: '123 10th ave', city: 'Denver', state: 'CO', zip_code: 80205, description: "I need cuddly pets", status: 'Pending')
+      @applicant_2 = Application.create!(name: 'John', address: '321 15th ave', city: 'Denver', state: 'CO', zip_code: 80205, description: "I need cuddly pets", status: 'Approved')
+      @applicant_3 = Application.create!(name: 'Charles', address: '678 street ave', city: 'Denver', state: 'CO', zip_code: 80205, description: "I need cuddly pets", status: 'Pending')
+
+
+      PetApplication.create(application: @applicant_1, pet: @pet_1)
+      PetApplication.create(application: @applicant_1, pet: @pet_2)
+      PetApplication.create(application: @applicant_2, pet: @pet_3)
+      PetApplication.create(application: @applicant_3, pet: @pet_3)
+
       visit '/admin/shelters'
     end
 
@@ -16,6 +27,15 @@ RSpec.describe 'As a visitor' do
 
       expect(@shelter_2.name).to appear_before(@shelter_3.name)
       expect(@shelter_3.name).to appear_before(@shelter_1.name)
+    end
+
+    it "shows section for names of Shelter's with Pending Applications" do
+      within("#pending-apps") do
+        # save_and_open_page
+        expect(page).to have_content("Shelters with Pending Applications")
+        expect(page).to have_content(@shelter_1.name)
+        expect(page).to have_content(@shelter_3.name)
+      end
     end
   end
 end
