5c9e432a599690509ab1532fcfa5c61c4f8ac781
Brisa Garcia <brisagarciaglz@gmail.com>
2021-04-04 11:46:53 -0600

    Add to index method

--- app/controllers/admin/shelters_controller.rb
+++ app/controllers/admin/shelters_controller.rb
@@ -1,5 +1,6 @@
 class Admin::SheltersController < ApplicationController
   def index
     @shelters = Shelter.desc_by_name
+    @shelters_with_pending_apps = Shelter.pending_applications
   end
 end
