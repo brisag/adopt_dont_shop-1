1871550f4a9d0d39043fd63a47adff16b81c1329
Brisa Garcia <brisagarciaglz@gmail.com>
2021-04-08 10:41:03 -0600

    Add stories for admin and app tests

--- spec/features/admin/application/show_spec.rb
+++ spec/features/admin/application/show_spec.rb
@@ -10,6 +10,9 @@ RSpec.describe 'As a visitor' do
       @pet1 = @shelter1.pets.create!(name: 'Francine', breed: 'dog', age: 1, adoptable: true)
       @pet2 = @shelter2.pets.create!(name: 'Joey', breed: 'dog', age: 3, adoptable: true)
       @pet3 = @shelter1.pets.create!(name: 'Mr. Dog', breed: 'cat', age: 10, adoptable: true)
+      @pet4 = @shelter1.pets.create!(name: 'Arthur', breed: 'dog', age: 1, adoptable: true)
+      @pet5 = @shelter2.pets.create!(name: 'Toy', breed: 'dog', age: 3, adoptable: true)
+      @pet6 = @shelter1.pets.create!(name: 'Mrs. Cat', breed: 'cat', age: 10, adoptable: true)
 
       @applicant1 = Application.create!(name: 'Brisa', address: '123 10th ave', city: 'Denver', state: 'CO', zip_code: 80205, description: "i need cuddly pets")
       @applicant2 = Application.create!(name: 'John', address: '321 Blake st', city: 'Denver', state: 'CO', zip_code: 80204, description: "i need cute pets")
@@ -109,6 +112,10 @@ RSpec.describe 'As a visitor' do
 
         within("#admin-applicant-#{@applicant2.id}") do
           within("#pet-#{@pet1.id}") do
+            click_button('Approve')
+          end
+
+          within("#pet-#{@pet2.id}") do
             click_button('Approve')
           end
           expect(page).to have_content('Accepted')
